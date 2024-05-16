11fe43d094f1222f674be237f3e0ba84e17723d3
fortune-0 <fortunepeterspc07@gmail.com>
2024-05-16 17:28:07 +0100

    updates added the create function on model/algo.py function takes in task/event, date and time

--- model/algo.py
+++ model/algo.py
@@ -1,14 +1,27 @@
 #!/usr/env/python3
+import datetime
+import json
 
 def create():
     task = input("Enter the task/event: ")
-    # date = input(" Enter the date ")
+    while True:
+        
+        try:
+            dat = input("Enter the date DD-MM: ")
+            datetime.datetime.strptime(dat, "%d-%m")
+            day, month = map(int, dat.split("-"))
+            if day < 31 and month < 12:
+                break
+            else:
+                print("Out of range, input a valid date")
+        except ValueError:
+            print("Invalid date format. Please enter date in DD-MM (Day & Month) format e.g 23-05")
+            continue
+        
     while True:
         tim = input("Enter the time HH:MM ")
         if len(tim) == 3 or len(tim) <2 or len(tim) > 5:
             print("Invalid time format. Please enter time HH:MM e.g 05:23")
-        elif len(tim) == 2:
-            print("Invalid time format. Please enter the minutes MM")
         else:
             try:
                 hours, minutes = map(int, tim.split(":"))
@@ -18,9 +31,27 @@ def create():
                     print("Invalid time format. Enter time in HH:MM e.g 05:23")
             except ValueError:
                 print("Invalid input,\n Enter time in number format.")
+    
+    print ("REMINDER CREATED SUCCESSFULLY")
+                
+    reminder = {
+            'task': task,
+            'date': dat,
+            'time': tim
+        }
+    
+    # reminder.append(reminder)
+    
+    with open("reminder.json",'w') as f:
+        json.dump(reminder, f, indent=4)
+        
+        print("Reminder Saved Successfully\n")
+                
+# def update(arg):
         
     
-    print(task)
+    print("\n",task)
     print(tim)
+    print(dat)
 # date = input("Enter the date (YYYY-MM-DD): ")
 create()
---
+++ test.py
@@ -0,0 +1,9 @@
+import datetime
+
+time = datetime.datetime.now()
+
+print(time)
+# print(time.year)
+# print(time.month)
+# print(time.hour,":",time.minute)
+print(time.day)
