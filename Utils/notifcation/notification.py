948a699f58f31ae27a76192783e313d7939ac45b
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-01 05:39:08 +0100

    notifications

--- Utils/notifcation/notification.py
+++ Utils/notifcation/notification.py
@@ -1,8 +1,11 @@
 from toastify import notify
 import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 import sqlite3
 
+notification_start_time = None
 def pull_reminder():
+     global notification_start_time
      conn = sqlite3.connect('promptly_db.sqlite')
      c = conn.cursor()
      
@@ -21,21 +24,28 @@ def pull_reminder():
           
           current_date = datetime.date.today()
           current_time = datetime.datetime.now().time()
-          
-          print(task)
+          # print(task)
      
           if date.date() == current_date and time.time() <= current_time:
-               notify(
-                    BodyText=task,
-                    AppName='Promptly app',
-                    AppPath='AppPath (Optional)',
-                    TitleText='TitleText (Optional)',
-                    ImagePath='icon.ico'
-               )
-          else:
-               # print(f"no current reminder now you have reminders set for {date.date()}, with time {time.time()}")
-if __name__ == "__main__":
-     pull_reminder()
+               if notification_start_time is None:
+                    notification_start_time = datetime.datetime.now()
+                    elapsed_time = datetime.datetime.now() - notification_start_time
+                    
+                    if elapsed_time.seconds < 300:
+                         notify(
+                              BodyText=task,
+                              AppName='Promptly app',
+                              AppPath='AppPath (Optional)',
+                              TitleText='Reminder',
+                              ImagePath='icon.ico'
+                              )
+                    else:
+                         print("Stopping notifications after 5 mins")
+                         notification_start_time = None
+                         print("No current reminder\n")
+               # print(f"No current reminder now you have reminders set for {date.date()}, with time {time.time()}")
+# if __name__ == "__main__":
+#      pull_reminder()
                
           # date, time = dat_tim.split
           # if date == time:
@@ -55,5 +65,10 @@ if __name__ == "__main__":
 # 	ImagePath='icon.ico)'
 # 	)
 if __name__ == "__main__":
-     pull_reminder()
+     scheduler = BlockingScheduler()
+     scheduler.add_job(pull_reminder, 'interval', minutes=1)
+     try:
+        scheduler.start()
+     except (KeyboardInterrupt, SystemExit):
+          pass
 # pull_reminder()
